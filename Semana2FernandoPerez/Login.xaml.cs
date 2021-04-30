d1a0457b8d49563653cc38d605f03d18a7fbb03b
ferjaviperez <78280871+ferjaviperez@users.noreply.github.com>
2021-04-30 00:23:44 -0500

    Initial commit

---
+++ Semana2FernandoPerez/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana2FernandoPerez
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string clave = txtPass.Text;
+            if (usuario != "estudiante2021" | clave != "uisrael2021")
+            {
+                await DisplayAlert("Alerta", "Usuario o contraseña incorrectos", "Cerrar");
+                return;
+            }
+
+            await DisplayAlert("Bienvenido", "Se ha validado el acceso", "Continuar");
+            await Navigation.PushAsync(new MainPage(usuario, clave));
+        }
+
+    }
+
+}
---
+++ Semana2FernandoPerez/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana2FernandoPerez
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(string usuario, string clave)
+        {
+            InitializeComponent();
+        }
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+         
+            try
+            {
+
+                double dato1 = Convert.ToDouble(txts1.Text);
+                double dato2 = Convert.ToDouble(txte1.Text);
+                double dato3 = Convert.ToDouble(txts2.Text);
+                double dato4 = Convert.ToDouble(txte2.Text);
+
+                double suma = ((dato1 * 0.3) + (dato2 * 0.2)) + ((dato3 * 0.3) + (dato4 * 0.2));
+                
+                txtpromedio.Text = suma.ToString()+ " Su Promedio";
+
+                if (suma >= 7)
+                {
+                    DisplayAlert("Mensaje", "APROBADO", "Cerrar");
+                }
+                else if (suma >= 5)
+                {
+                    DisplayAlert("Mensaje", "COMPLEMENTARIO", "Cerrar");
+                }
+                else if (suma < 5)
+                {
+                    DisplayAlert("Mensaje", "REROBADO", "Cerrar");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //Mensaje de erro capturado por el catch
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+
+        }
+    }
+}
